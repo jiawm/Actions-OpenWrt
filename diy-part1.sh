c8abb132ae7cbf5210e4279d977040da38f19a33
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-08 18:05:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,25 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git packages https://github.com/coolsnowwolf/packages
+#sed -i '$a src-git luci https://github.com/coolsnowwolf/luci
+#sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git
+#sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages
+
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/lienol
+#git clone https://github.com/fw876/helloworld.git package/hellowrold
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
+git clone https://github.com/jiawm/luci-app-poweroff.git package/poweroff
+git clone https://github.com/linkease/nas-packages.git package/nas #添加ddnsto和linkease
+
+#替换为sirpdboy中文版netdata
+rm -rf ./package/lean/luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git package/lean/luci-app-netdata
+
