bb05f6c47655673385fd892a94b92a1ab94a5db6
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-20 19:20:29 +0800

    Update diy-part2-x86-sirpdboy.sh

--- Scripts/diy-part2-x86-sirpdboy.sh
+++ Scripts/diy-part2-x86-sirpdboy.sh
@@ -23,6 +23,8 @@ rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
 
+# Change default BackGround img
+wget -O ./package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://github.com/xylz0928/Openwrt-Make-x86_7621/raw/main/BigSurLight.jpg
 
 #git lua-maxminddb 依赖
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
