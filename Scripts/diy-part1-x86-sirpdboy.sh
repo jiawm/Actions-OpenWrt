0ed15a7a98c82e14686f0e9fa344db519b8a129f
jiawm <42288069+jiawm@users.noreply.github.com>
2021-05-26 19:28:00 +0800

    Update diy-part1-x86-sirpdboy.sh

--- Scripts/diy-part1-x86-sirpdboy.sh
+++ Scripts/diy-part1-x86-sirpdboy.sh
@@ -19,6 +19,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/lienol
 git clone https://github.com/tty228/luci-app-serverchan.git package/custom/serverchan
 git clone https://github.com/jiawm/luci-app-poweroff.git package/poweroff
 git clone https://github.com/linkease/nas-packages.git package/nas #添加ddnsto和linkease
-git clone https://github.com/jiawm/sirpdboy-package.git package/sirpdboy
+git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy
 #替换为sirpdboy中文版netdata
 rm -rf ./package/lean/luci-app-netdata && git clone https://github.com/sirpdboy/luci-app-netdata.git package/lean/luci-app-netdata
