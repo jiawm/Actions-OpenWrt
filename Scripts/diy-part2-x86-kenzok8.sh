d2efb74cfc086737b583cf1775c4f9b090f4b9e8
jiawm <42288069+jiawm@users.noreply.github.com>
2021-12-19 15:48:14 +0800

    Update diy-part2-x86-kenzok8.sh

--- Scripts/diy-part2-x86-kenzok8.sh
+++ Scripts/diy-part2-x86-kenzok8.sh
@@ -12,10 +12,10 @@
 
 
 # 切换ramips内核为 5.10
-sed -i 's/5.4/5.10/g' ./target/linux/ramips/Makefile
+# sed -i 's/5.4/5.10/g' ./target/linux/ramips/Makefile
 
 # 切换x86内核为 5.10
-sed -i 's/5.4/5.10/g' ./target/linux/x86/Makefile
+# sed -i 's/5.4/5.10/g' ./target/linux/x86/Makefile
 
 # 添加温度显示
 sed -i 's/invalid/# invalid/g' package/network/services/samba36/files/smb.conf.template
