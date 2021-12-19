c8410089fac878b01a725ad669acd5bf1e6d03e9
jiawm <42288069+jiawm@users.noreply.github.com>
2021-12-19 15:19:19 +0800

    Update diy-part2-x86-kenzok8.sh

--- Scripts/diy-part2-x86-kenzok8.sh
+++ Scripts/diy-part2-x86-kenzok8.sh
@@ -31,7 +31,7 @@ sed -i 's/bootstrap/opentopd/g' feeds/luci/modules/luci-base/root/etc/config/luc
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 
 # Change default BackGround img
 # wget -O ./package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg https://github.com/jiawm/My-OpenWrt-by-Lean/raw/main/BackGround/2.jpg
