84c6ac431cef8e12b82962496862aca4392703a1
jiawm <42288069+jiawm@users.noreply.github.com>
2021-12-22 16:57:25 +0800

    Update diy-part2-x86-kenzok8.sh

--- Scripts/diy-part2-x86-kenzok8.sh
+++ Scripts/diy-part2-x86-kenzok8.sh
@@ -35,7 +35,7 @@ sed -i 's/bootstrap/argon/g' feeds/luci/modules/luci-base/root/etc/config/luci
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 #添加argon主题设置插件
-git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config
 
 
 # Change default BackGround img
